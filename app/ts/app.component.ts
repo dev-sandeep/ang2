4d0ccbe32417249360489dd9f787a017c085c77e
sandeep gantait <sandeep.gantait@sap.com>
2016-09-20 18:54:47 +0530

    adding all the files

---
+++ app/ts/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from './playlist.component';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{ mainHeading }}</h1>'
+})
+
+export class AppComponent {
+    mainHeading = Config.MAIN_HEADING;
+}
