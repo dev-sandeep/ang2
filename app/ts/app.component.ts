5b1a223847f9526ed9933989a6eaf36493d76360
Sandeep <dev.sandeep@hotmail.com>
2016-09-20 22:59:19 +0530

    [M] boston tut complete

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -5,9 +5,18 @@ import {PlaylistComponent} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{ mainHeading }}</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives : [PlaylistComponent]
 })
 
 export class AppComponent {
     mainHeading = Config.MAIN_HEADING;
+    videos : Array<Video>;
+    
+    constructor(){
+        this.videos = [
+            new Video(1, "Video 1", "PF0Haq0ftyw", "Video 1 description goes here"),
+            new Video(1, "Video 2", "nOK3egx58CE", "Video 2 description goes here"),
+        ];
+    }
 }
--- app/ts/playlist.component.ts
+++ app/ts/playlist.component.ts
@@ -1,10 +1,14 @@
 import {Component} from 'angular2/core';
+import {Video} from './video';
 
 @Component({
     selector: 'playlist',
-    template: '<h1>{{ mainHeading }}</h1>'
+    templateUrl: '/app/ts/playlist.component.html',
+    inputs : ['videos']
 })
 
 export class PlaylistComponent {
-  
+    onSelectVideo(vid:Video){
+        console.log(vid);
+    }
 }
